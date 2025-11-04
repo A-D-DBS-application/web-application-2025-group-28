b4523de461e92d787040602f81a0a12f4823eb2d
Juttasw <jutta.swerts@ugent.be>
2025-11-04 11:36:27 +0100

    Dashboard begin toegevoegd

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)  # <-- this defines the Flask app that Flask looks for
+app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, Flask is running successfully"
+def dashboard():
+    total_items = 230
+    inspection_items = 5
+    return render_template('dashboard.html',
+                           total_items=total_items,
+                           inspection_items=inspection_items)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
