4471f1f96f3ee8122f371e4a00334a29a5db2046
Juttasw <jutta.swerts@ugent.be>
2025-11-04 13:23:24 +0100

    functionaliteiten andere pagina's

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Dummy data
+materiaal_items = 230
+keuring_items = 5
+
 @app.route('/')
 def dashboard():
-    total_items = 230
-    inspection_items = 5
-    return render_template('dashboard.html',
-                           total_items=total_items,
-                           inspection_items=inspection_items)
+    query = request.args.get('query', '')
+    return render_template(
+        'dashboard.html',
+        materiaal_items=materiaal_items,
+        keuring_items=keuring_items,
+        query=query
+    )
+
+@app.route('/materiaal')
+def materiaal():
+    return render_template('materiaal.html')
+
+@app.route('/keuringen')
+def keuringen():
+    return render_template('keuringen.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
